b2a4f6973d3e0a6c6e439a33c391c20ec2589ab4
https://github.com/Nguyenhuy18062006/Danh-X-O.git <huyn65830@gmail.com>
2026-03-11 12:36:49 +0700

    commit lan dau

---
+++ script.js
@@ -0,0 +1,57 @@
+const slider = document.getElementById("slider");
+const cardWidth = 270;
+
+// nhân đôi card
+slider.innerHTML += slider.innerHTML;
+
+slider.style.scrollBehavior = "smooth";
+
+let isScrolling = false;
+
+function checkLoop() {
+  if (slider.scrollLeft >= slider.scrollWidth / 2) {
+    slider.style.scrollBehavior = "auto";
+    slider.scrollLeft -= slider.scrollWidth / 2;
+    slider.style.scrollBehavior = "smooth";
+  }
+  if (slider.scrollLeft < 0) {
+    slider.style.scrollBehavior = "auto";
+    slider.scrollLeft += slider.scrollWidth / 2;
+    slider.style.scrollBehavior = "smooth";
+  }
+}
+
+let animationId;
+let userInteracting = false;
+
+function autoSlide() {
+  if (!userInteracting) {
+    slider.scrollLeft += 0.5;
+    checkLoop();
+  }
+  animationId = requestAnimationFrame(autoSlide);
+}
+
+autoSlide();
+
+// nút next
+document.querySelector(".next").onclick = function () {
+  userInteracting = true;
+  slider.style.scrollBehavior = "smooth";
+  slider.scrollLeft += cardWidth;
+  setTimeout(() => {
+    checkLoop();
+    userInteracting = false;
+  }, 400);
+};
+
+// nút prev
+document.querySelector(".prev").onclick = function () {
+  userInteracting = true;
+  slider.style.scrollBehavior = "smooth";
+  slider.scrollLeft -= cardWidth;
+  setTimeout(() => {
+    checkLoop();
+    userInteracting = false;
+  }, 400);
+};
